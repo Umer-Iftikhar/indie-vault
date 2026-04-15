f398e010278581a46b3b5fa1d018e898f48baa30
Umer-Iftikhar <umeriftikhar981@gmail.com>
2026-04-15 20:30:21 +0500

    Initial MVC project setup - Phase 8 start

---
+++ IndieGamePlatform/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IndieGamePlatform.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
