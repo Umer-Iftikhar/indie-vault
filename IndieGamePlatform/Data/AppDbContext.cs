8596fcc1b581b86a182d5e66bfe6da31270b9859
Umer-Iftikhar <umeriftikhar981@gmail.com>
2026-04-17 12:33:50 +0500

    Created AppDbContext and DbSets of all models

---
+++ IndieGamePlatform/Data/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using IndieGamePlatform.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace IndieGamePlatform.Data
+{
+    public class AppDbContext : Microsoft.AspNetCore.Identity.EntityFrameworkCore.IdentityDbContext<ApplicationUser>
+    {
+        public  AppDbContext (DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Game> Games { get; set; } = null!;
+        public DbSet<Review> Reviews { get; set; } = null!;
+        public DbSet<Wishlist> Wishlists { get; set; } = null!;
+        public DbSet<DownloadHistory> DownloadHistories { get; set; } = null!;
+        public DbSet<Genre> Genres { get; set; } = null!;
+        public DbSet<GameTag> GameTags { get; set; } = null!;
+        public DbSet<Tag> Tags { get; set; } = null!;
+        public DbSet<Screenshot> Screenshots { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<GameTag>().HasKey(gt => new { gt.GameId, gt.TagId });
+        }
+    }
+}
