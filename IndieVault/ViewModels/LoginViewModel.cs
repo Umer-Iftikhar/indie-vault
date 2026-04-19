c448f23a6cbd2a34b075f112f7dca69fb4fc4ae5
Umer-Iftikhar <umeriftikhar981@gmail.com>
2026-04-19 14:24:35 +0500

    Created viewmodels for login and registration

--- IndieVault/ViewModels/LoginViewModel.cs
+++ IndieVault/ViewModels/LoginViewModel.cs
@@ -1,6 +1,18 @@
-﻿namespace IndieVault.ViewModels
+﻿using IndieVault.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace IndieVault.ViewModels
 {
     public class LoginViewModel
     {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        [DataType(DataType.Password)]
+        [MinLength(6, ErrorMessage = "Password must be at least 6 characters long.")]
+        public string Password { get; set; } = string.Empty;
+        
+        public bool RememberMe { get; set; }
     }
 }
--- IndieVault/ViewModels/RegisterViewModel.cs
+++ IndieVault/ViewModels/RegisterViewModel.cs
@@ -1,6 +1,21 @@
-﻿namespace IndieVault.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IndieVault.ViewModels
 {
     public class RegisterViewModel
     {
+        [Required]
+        [MinLength(2, ErrorMessage = "Name must be at least 2 characters long.")]
+        public string UserName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords do not match.")]
+        public string ConfirmPassword { get; set; } = string.Empty;
     }
 }
